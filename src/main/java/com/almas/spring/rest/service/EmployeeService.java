836e4e62561c6c19940c199e6dbe1bf925ed3168
alimkulov <emerald@internet.ru>
2023-06-11 12:32:14 +0600

    test

---
+++ src/main/java/com/almas/spring/rest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.almas.spring.rest.service;
+
+import com.almas.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployee();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
